94fd8e316f5f37160ab707703a05b2fedfdf93c6
Daniela-Prada <daniela.prada@skyline.be>
2024-08-20 14:55:41 -0500

    Upgrade nugget interapp

--- EPMBeToOltPassives_1/obj/Debug/net48/EPMBeToOltPassives_1.AssemblyInfo.cs
+++ EPMBeToOltPassives_1/obj/Debug/net48/EPMBeToOltPassives_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EPMBeToOltPassives_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75dcfec8ea649e9d5d7119df686192dd135007c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EPMBeToOltPassives_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EPMBeToOltPassives_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
